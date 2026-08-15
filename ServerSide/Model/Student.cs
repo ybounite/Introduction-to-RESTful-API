78f6b985e653a8d7b56c4ddc8c52b9ffe1b9bdad
ybounite <dimayoussef42@gmail.com>
2026-08-15 16:35:07 +0100

    Remove Student model to streamline API structure

--- ServerSide/Model/Student.cs
+++
@@ -1,9 +0,0 @@
-namespace TestApp.Model;
-
-public class Student
-{
-  public int Id { get; set; }
-  public string Name { get; set; } = String.Empty;
-  public int Age { get; set; }
-  public int Grade { get; set; }
-}
