9c7f646557e584ee4a3fb9910ce389a8ffca3054
ybounite <dimayoussef42@gmail.com>
2026-09-17 18:05:23 +0100

    feat: Add data annotations for email and password in LoginRequest DTO

--- SecursAPIs/ServerSide/StudentApiBusinessLayer/DTOs/Auth/LoginDTO.cs
+++ SecursAPIs/ServerSide/StudentApiBusinessLayer/DTOs/Auth/LoginDTO.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace StudentApiBusinessLayer.DTOs.Auth;
 
 public class LoginRequest
 {
+  [Required]
+  [EmailAddress]
   public string Email {get; set;} = string.Empty;
+  [Required]
   public string Password {get; set;} = string.Empty;
 }
